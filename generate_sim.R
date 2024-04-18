e0f06a2fe187ffa76da172904039bb002a5caba6
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-04-18 14:17:09 +0100

    generate_sim.R

--- generate_sim.R
+++ generate_sim.R
@@ -1,4 +1,4 @@
- ## Auxiliary function to simulate logistic data.
+## Auxiliary function to simulate logistic data.
 expit <- function (x) exp(x) / (1 + exp(x))
 
 ## Set up the simulation - set sample size and common cause.
@@ -23,6 +23,8 @@ progression.probs <- expit(-1 + as.vector(G %*% progression.betas) + 1 * U)
 P <- rbinom(n, 1, progression.probs)
 
 ##Simulate GWAS of incidence.
+#Include effects of progression SNPs on incidence and vice versa to conduct subsequent analyses.
+
 
 incidence_GWAS <- matrix(0, length(incidence.SNPs), 3); colnames(incidence_GWAS) <- c("Estimate", "StdErr", "Pval")
 
@@ -33,11 +35,25 @@ for (j in incidence.SNPs) {
   incidence_GWAS[j, 3] <- summary(incidence_model)$coefficients["G[, j]", "Pr(>|z|)"]
 }
 
-##Simulate GWAS of progression. Creating a matrix of length progression.SNPs causes an error - the matrix will fill from 91:100, need to fix this.
+for (j in progression.SNPs) {
+  incidence_model <- glm(I ~ G[,j], family = binomial)
+  incidence_GWAS[j, 1] <- summary(incidence_model)$coefficients["G[, j]", "Estimate"]
+  incidence_GWAS[j, 2] <- summary(incidence_model)$coefficients["G[, j]", "Std. Error"]
+  incidence_GWAS[j, 3] <- summary(incidence_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+##Simulate GWAS of progression.
 
 progression_GWAS <- matrix(0, 100, 3); colnames(progression_GWAS) <- c("Estimate", "StdErr", "Pval")
 
 for (j in progression.SNPs) {
+  progression_model <- glm(P ~ G[,j], family = binomial)
+  progression_GWAS[j, 1] <- summary(progression_model)$coefficients["G[, j]", "Estimate"]
+  progression_GWAS[j, 2] <- summary(progression_model)$coefficients["G[, j]", "Std. Error"]
+  progression_GWAS[j, 3] <- summary(progression_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+for (j in incidence.SNPs) {
   progression_model <- glm(P ~ G[,j], family = binomial)
   progression_GWAS[j, 1] <- summary(progression_model)$coefficients["G[, j]", "Estimate"]
   progression_GWAS[j, 2] <- summary(progression_model)$coefficients["G[, j]", "Std. Error"]
