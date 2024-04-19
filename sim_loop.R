ff8d744ba6db6aea287ad97dbddc6d5676885cf2
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-04-19 12:35:24 +0100

    sim_loop.R

--- sim_loop.R
+++ sim_loop.R
@@ -1,1 +1,54 @@
+iter <- 1000
 
+loop_results <- list()
+
+for (i in 1:iter) {
+  
+##Simulate common cause (collider).
+U <- rnorm(n, 0, 1)
+
+##Simulate one-sample genetic data from individual-level data - number of SNPs, simulate MAF, ratios for how many affect I and P.
+maf <- runif(nSNPs, 0.05, 0.5)
+for (j in 1:nSNPs) G[, j] <- rbinom(n, 2, maf[j])
+incidence.betas[incidence.SNPs] <- rnorm(length(incidence.SNPs), 0, 0.2)
+incidence.probs <- expit(-1 + as.vector(G %*% incidence.betas) + 1 * U)
+I <- rbinom(n, 1, incidence.probs)
+progression.betas[progression.SNPs] <- rnorm(length(progression.SNPs), 0, 0.2)
+progression.probs <- expit(-1 + as.vector(G %*% progression.betas) + 1 * U)
+P <- rbinom(n, 1, progression.probs)
+
+##Simulate GWAS of incidence.
+  
+for (j in incidence.SNPs) {
+  incidence_model <- glm(I ~ G[,j], family = binomial)
+  incidence_GWAS[j, 1] <- summary(incidence_model)$coefficients["G[, j]", "Estimate"]
+  incidence_GWAS[j, 2] <- summary(incidence_model)$coefficients["G[, j]", "Std. Error"]
+  incidence_GWAS[j, 3] <- summary(incidence_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+for (j in progression.SNPs) {
+  incidence_model <- glm(I ~ G[,j], family = binomial)
+  incidence_GWAS[j, 1] <- summary(incidence_model)$coefficients["G[, j]", "Estimate"]
+  incidence_GWAS[j, 2] <- summary(incidence_model)$coefficients["G[, j]", "Std. Error"]
+  incidence_GWAS[j, 3] <- summary(incidence_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+##Simulate GWAS of progression.
+  
+for (j in progression.SNPs) {
+  progression_model <- glm(P ~ G[,j], family = binomial)
+  progression_GWAS[j, 1] <- summary(progression_model)$coefficients["G[, j]", "Estimate"]
+  progression_GWAS[j, 2] <- summary(progression_model)$coefficients["G[, j]", "Std. Error"]
+  progression_GWAS[j, 3] <- summary(progression_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+for (j in incidence.SNPs) {
+  progression_model <- glm(P ~ G[,j], family = binomial)
+  progression_GWAS[j, 1] <- summary(progression_model)$coefficients["G[, j]", "Estimate"]
+  progression_GWAS[j, 2] <- summary(progression_model)$coefficients["G[, j]", "Std. Error"]
+  progression_GWAS[j, 3] <- summary(progression_model)$coefficients["G[, j]", "Pr(>|z|)"]
+}
+
+results[[i]] <- list(incidence_GWAS = incidence_GWAS, progression_GWAS = progression_GWAS)
+  
+}
