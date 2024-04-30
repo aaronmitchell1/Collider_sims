035d1bfe8abe8880a39e6a3dbd64b21702fb1de3
aaronmitchell1 <112105321+aaronmitchell1@users.noreply.github.com>
2024-04-30 20:17:07 +0100

    Update sim_loop.R
    Added MR-Horse and call R2Jags for HPC

--- sim_loop.R
+++ sim_loop.R
@@ -8,6 +8,55 @@ progression.SNPs <- 91:100
 library(MendelianRandomization)
 library(mr.raps)
 library(SlopeHunter)
+library(R2jags)
+
+mr_horse = function(D, no_ini = 3, variable.names = "theta", n.iter = 10000, n.burnin = 10000){
+  if("theta" %in% variable.names){
+    variable.names = variable.names
+  } else{
+    variable.names = c("theta", variable.names)
+  }
+  jags_fit = jags(data = list(by = D$betaY, bx = D$betaX, sy = D$betaYse, sx = D$betaXse, N = length(D$betaY)),
+                  parameters.to.save = variable.names,
+                  n.chains = no_ini,
+                  n.iter = n.burnin + n.iter,
+                  n.burnin = n.burnin,
+                  model.file = mr_horse_model)
+  mr.coda = as.mcmc(jags_fit)
+  mr_estimate = data.frame("Estimate" = round(unname(summary(mr.coda[, "theta"])$statistics[1]), 3),
+                           "SD" = round(unname(summary(mr.coda[, "theta"])$statistics[2]), 3),
+                           "2.5% quantile" = round(unname(summary(mr.coda[, "theta"])$quantiles[1]), 3),
+                           "97.5% quantile" = round(unname(summary(mr.coda[, "theta"])$quantiles[5]), 3),
+                           "Rhat" = round(unname(gelman.diag(mr.coda)$psrf[1]), 3))
+  names(mr_estimate) = c("Estimate", "SD", "2.5% quantile", "97.5% quantile", "Rhat")
+  return(list("MR_Estimate" = mr_estimate, "MR_Coda" = mr.coda))
+}
+
+mr_horse_model = function() {
+  for (i in 1:N){
+    by[i] ~ dnorm(mu[i], 1/(sy[i] * sy[i]))
+    mu[i] = theta * bx0[i] + alpha[i]
+    bx[i] ~ dnorm(bx0[i], 1 / (sx[i] * sx[i]))
+    
+    bx0[i] ~ dnorm(mx0 + (sqrt(vx0)/(tau * phi[i])) * rho[i] * alpha[i], 1 / ((1 - rho[i]^2) * vx0))
+    r[i] ~ dbeta(10, 10);T(, 1)
+    rho[i] = 2*r[i] -1
+    
+    alpha[i] ~ dnorm(0, 1 / (tau * tau * phi[i] * phi[i]))
+    phi[i] = a[i] / sqrt(b[i])
+    a[i] ~ dnorm(0, 1);T(0, )
+    b[i] ~ dgamma(0.5, 0.5)
+  }
+  
+  c ~ dnorm(0, 1);T(0, )
+  d ~ dgamma(0.5, 0.5)
+  tau = c / sqrt(d)
+  
+  vx0 ~ dnorm(0, 1);T(0, )
+  mx0 ~ dnorm(0, 1)
+  
+  theta ~ dunif(-10, 10)
+}
 
 loop_methods <- vector('list', iter)
 incidence_GWAS <- array(0, dim = c(iter, nSNPs, 3))
